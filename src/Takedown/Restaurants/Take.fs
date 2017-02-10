edfc3a3adecf509b66e35aaf6b0c9565eda8bc96
Magnus Hoff <maghoff@gmail.com>
2017-02-10 11:40:05 +0100

    Handle weird Take markup. Implemented with ❤️ and coincidence

--- src/Takedown/Restaurants/Take.fs
+++ src/Takedown/Restaurants/Take.fs
@@ -22,11 +22,21 @@ let parsePrice (node : HtmlNode) =
 let mapMenuEntry node =
     let bs = node |> descendants "b" |> Seq.toList
     match bs with
+    | headerNode::priceNode::headerNode2::priceNode2::_ ->
+        [
+            (match mapMenuItemHeader headerNode with
+            | Some (number, name) -> Some { Number = number; Name = name.ToLower(); Price = parsePrice priceNode }
+            | None -> None)
+        ;
+            (match mapMenuItemHeader headerNode2 with
+            | Some (number, name) -> Some { Number = number; Name = name.ToLower(); Price = parsePrice priceNode2 }
+            | None -> None)
+        ] |> Seq.choose id |> Seq.toList
     | headerNode::priceNode::_ ->
         match mapMenuItemHeader headerNode with
-        | Some (number, name) -> Some { Number = number; Name = name.ToLower(); Price = parsePrice priceNode }
-        | None -> None
-    | _ -> None
+        | Some (number, name) -> [ { Number = number; Name = name.ToLower(); Price = parsePrice priceNode } ] |> Seq.toList
+        | None -> [] |> Seq.toList
+    | _ -> [] |> Seq.toList
 
 let menuCategory node =
     let xpath = "//div[@class='box box_3']//div[@align='center']"
@@ -39,7 +49,10 @@ let menuEntries node =
     let menuItemNodes = node |> selectNodes xpath
     { 
         Category = menuCategory node
-        Entries =  menuItemNodes |> Seq.choose mapMenuEntry |> Seq.sortBy (fun x -> x.Number)
+        Entries =  menuItemNodes
+            |> Seq.map mapMenuEntry
+            |> Seq.concat
+            |> Seq.sortBy (fun x -> x.Number)
     }
     
 let menyUrls doc = 
