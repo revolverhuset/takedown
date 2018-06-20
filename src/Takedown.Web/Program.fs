344806d5f845f25fd96406e0e50011731cdd27b6
Håvard Sørbø <havard@hsorbo.no>
2018-06-21 01:59:23 +0200

    initial "rest"-service

---
+++ src/Takedown.Web/Program.fs
@@ -0,0 +1,55 @@
+module TakeDown.Web
+
+open System
+open System.IO
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.Logging
+open Microsoft.Extensions.DependencyInjection
+open Giraffe
+
+let indexHandler (name : string) =
+    text <| sprintf "Hello %s, from Giraffe!" name
+
+let webApp =
+    choose [
+        GET >=>
+            choose [
+                route "/take" >=> json (Takedown.Restaurants.Take.takeDown ())
+                route "/" >=> indexHandler "world"
+                routef "/hello/%s" indexHandler
+            ]
+        setStatusCode 404 >=> text "Not Found" ]
+
+let errorHandler (ex : Exception) (logger : ILogger) =
+    logger.LogError(EventId(), ex, "An unhandled exception has occurred while executing the request.")
+    clearResponse >=> setStatusCode 500 >=> text ex.Message
+
+
+let configureApp (app : IApplicationBuilder) =
+    let env = app.ApplicationServices.GetService<IHostingEnvironment>()
+    (match env.IsDevelopment() with
+    | true  -> app.UseDeveloperExceptionPage()
+    | false -> app.UseGiraffeErrorHandler errorHandler)
+        .UseGiraffe(webApp)
+
+let configureServices (services : IServiceCollection) =
+    services.AddGiraffe() |> ignore
+
+let configureLogging (builder : ILoggingBuilder) =
+    let filter (l : LogLevel) = l.Equals LogLevel.Error
+    builder.AddFilter(filter).AddConsole().AddDebug() |> ignore
+
+[<EntryPoint>]
+let main _ =
+    let contentRoot = Directory.GetCurrentDirectory()
+    WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(contentRoot)
+        .UseIISIntegration()
+        .Configure(Action<IApplicationBuilder> configureApp)
+        .ConfigureServices(configureServices)
+        .ConfigureLogging(configureLogging)
+        .Build()
+        .Run()
+    0
