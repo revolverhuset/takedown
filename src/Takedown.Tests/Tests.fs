4b66e199bd025d8d7a98910a0103bde7626623d1
Håvard Sørbø <havard@hsorbo.no>
2018-06-21 01:21:31 +0200

    Unit test some known menu-item-numbers

--- src/Takedown.Tests/Tests.fs
+++ src/Takedown.Tests/Tests.fs
@@ -1,8 +1,23 @@
 module Tests
-
+open Takedown.Restaurants
 open System
 open Xunit
 
-[<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+module TakeTests = 
+    [<Fact>]
+    let ``Known menuitems`` () =
+        let someKnowns = [
+            [101..113]
+            [115..116]
+            [201..206]
+            [301..304]
+            [401..406]
+            [501..519]
+            [601..649]] 
+        let found = 
+            Take.takeDown () 
+            |> Seq.collect (fun x -> x.Entries)
+            |> Seq.map (fun x -> x.Number)
+            |> Seq.sort
+            |> Seq.toList
+        Assert.Empty(someKnowns |> List.collect id |> List.except found)
